3e3117bd16d7c9bf68f3f8c5b81987c719ca6625
arnelimperial <9pagemake7@gmail.com>
2025-09-21 17:21:18 +0800

    refactor for ms1

--- js/configuration.example.js
+++
@@ -1,5 +0,0 @@
-// ➤ Rename to configuration.js and add your own EmailJS credentials
-
-export const EMAILJS_PUBLIC_KEY = "your_emailjs_public_key";
-export const EMAILJS_SERVICE_ID = "your_emailjs_service_id";
-export const EMAILJS_TEMPLATE_ID = "your_emailjs_template_id";
